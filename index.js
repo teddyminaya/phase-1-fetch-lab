b4cecc504bf0bcfe6dbc82b29e5e095e0f9e5afc
Teddy Minaya <teddyminaya@gmail.com>
2023-01-14 02:58:40 -0500

    commit tree 1d3d17bcc43d2315ab24a487d0eeced822ab8725

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(books => renderBooks(books))
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +17,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
